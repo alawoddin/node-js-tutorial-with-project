7b9f65655f81665df12958c8330c6e8a6f95e15b
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-05-02 22:58:57 +0430

    the firt lec of node js is doen

---
+++ lec1/play.js
@@ -0,0 +1,17 @@
+var name = "alawoddin"
+
+var age = 25
+
+var hobbies = true
+
+console.log(name)
+
+
+function details(username , userage , userjob) {
+    return (
+        "your name is " + username + " and your age is " + userage + " and your job is " + userjob
+    )
+}
+
+// console.log(details(name , age , hobbies)
+console.log(details("alawoddin" , 25 , "developer"))
