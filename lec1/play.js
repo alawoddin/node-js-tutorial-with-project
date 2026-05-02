a2dd380710f0a261b27779f1524a3a2f8dd4b5ad
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-05-02 23:04:14 +0430

    the first lec section is done

--- lec1/play.js
+++ lec1/play.js
@@ -1,17 +1,34 @@
-var name = "alawoddin"
+// var name = "alawoddin"
 
-var age = 25
+// var age = 25
 
-var hobbies = true
+// var hobbies = true
 
-console.log(name)
+// console.log(name)
 
 
-function details(username , userage , userjob) {
-    return (
-        "your name is " + username + " and your age is " + userage + " and your job is " + userjob
+// function details(username , userage , userjob) {
+//     return (
+//         "your name is " + username + " and your age is " + userage + " and your job is " + userjob
+//     )
+// }
+
+// // console.log(details(name , age , hobbies)
+// console.log(details("alawoddin" , 25 , "developer"))
+
+
+
+function userdetails(name , age , job) {
+    return(
+        'your name is ' + name + ' your age is ' + age + ' and your job is ' + job
     )
 }
 
-// console.log(details(name , age , hobbies)
-console.log(details("alawoddin" , 25 , "developer"))
+console.log(userdetails('alwoddin' , 24 , 'developer'))
+
+
+var title = 'node js '
+
+var info = 'this is the start '
+
+console.log(title , info)
