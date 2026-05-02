d2d6444d4dea2c4620d7fb717672cacbfeb1791f
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-05-02 23:11:46 +0430

    the arrow function is done

--- lec1/play.js
+++ lec1/play.js
@@ -1,34 +1,43 @@
-// var name = "alawoddin"
+var name = "alawoddin"
 
-// var age = 25
+var age = 25
 
-// var hobbies = true
+var hobbies = true
 
-// console.log(name)
+console.log(name)
 
-
-// function details(username , userage , userjob) {
-//     return (
-//         "your name is " + username + " and your age is " + userage + " and your job is " + userjob
-//     )
-// }
-
-// // console.log(details(name , age , hobbies)
-// console.log(details("alawoddin" , 25 , "developer"))
-
-
-
-function userdetails(name , age , job) {
-    return(
-        'your name is ' + name + ' your age is ' + age + ' and your job is ' + job
+const details = (username , userage , userjob) =>  {
+    return (
+        "your name is " + username + " and your age is " + userage + " and your job is " + userjob
     )
 }
 
-console.log(userdetails('alwoddin' , 24 , 'developer'))
+const add = (a , b) => {
+    return a + b
+}
+
+console.log(add(2,3))
+
+const add = (a,b) => a + b;
+console.log(add(3,3))
 
 
-var title = 'node js '
+// console.log(details(name , age , hobbies)
+console.log(details("alawoddin" , 25 , "developer"))
 
-var info = 'this is the start '
 
-console.log(title , info)
+
+// function userdetails(name , age , job) {
+//     return(
+//         'your name is ' + name + ' your age is ' + age + ' and your job is ' + job
+//     )
+// }
+
+// console.log(userdetails('alwoddin' , 24 , 'developer'))
+
+
+// var title = 'node js '
+
+// var info = 'this is the start '
+
+// console.log(title , info)
