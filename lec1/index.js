cd30f4641311bd735380dfacb0a71455b5f9c9f6
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-05-02 23:23:43 +0430

    the object section is done

--- lec1/index.js
+++ lec1/index.js
@@ -6,19 +6,30 @@
 // console.log(Person)
 
 
-const Person = {
-    name : "alawoddin", 
-    age : 20,
+// const Person = {
+//     name : "alawoddin", 
+//     age : 20,
+//     greet: function() {
+//         console.log("Hi I am " , this.name)
+        
+//     }
+//     greet() {
+//         console.log("Hi I am " , this.name)
+        
+//     }
+// }
+
+
+// // console.log(Person)
+// Person.greet()
+
+const Car = {
+    brand: "LX-570",
+    Model: 2020,
     greet: function() {
-        console.log("Hi I am " , this.name)
-        
-    }
-    greet() {
-        console.log("Hi I am " , this.name)
-        
+        console.log("you Car name " ,  this.brand , "and the model is " , this.Model)
     }
 }
 
-
-// console.log(Person)
-Person.greet()
+// console.log(Car)
+Car.greet()
