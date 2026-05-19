d902eefa4cbd40969a1e3d7c5a354b6cf756d865
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-05-19 12:54:28 +0430

    the aync section is done

---
+++ lec1/aync.js
@@ -0,0 +1,26 @@
+const fetchData = () => {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("done");
+        }, 1500);
+    });
+
+    return promise;
+};
+
+setTimeout(() => {
+    console.log("hello world");
+
+    fetchData()
+        .then(text => {
+            console.log(text);
+            return fetchData();
+        })
+        .then(text2 => {
+            console.log(text2);
+        });
+
+}, 2000);
+
+console.log("hello");
+console.log("hi");
